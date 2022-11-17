e11103488c1e2e42c00bdd7afed85fb50a81aa61
KihaJeong1994 <jkha7371@gmail.com>
2022-11-17 14:01:44 +0900

    entitygrpah & fetch batch 옵션으로 n+1 완화

--- src/main/java/com/example/jpa/repository/TeamRepository.java
+++ src/main/java/com/example/jpa/repository/TeamRepository.java
@@ -2,6 +2,8 @@ package com.example.jpa.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 
@@ -9,5 +11,9 @@ import com.example.jpa.entity.Team;
 
 public interface TeamRepository extends CrudRepository<Team,Long>, QueryByExampleExecutor<Team>{
     List<Team> findByPersonsLastname(String lastname);
+    @EntityGraph(type = EntityGraphType.LOAD,attributePaths = {"persons.colCur.classGrp","persons.acaCur.classGrp"})
+     //@EntityGraph, fetch join, queryDsl 모두 다 multi fetch join(MultipleBagFetchException)문제를 해결할 수 없음
+     //@EntityGraph, fetch join으로 MultipleBagFetchException이 발생하지 않는 범위 내에서 쿼리를 조정 후
+     // hibernate의 default_batch_fetch_size 기능을 사용해서 in 기능을 통해 쿼리를 줄여야함
     List<Team> findAll();
 }
