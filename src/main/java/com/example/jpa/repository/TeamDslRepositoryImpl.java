fa7c2fedb2389a2eb2617939ed5cecbdb6b7f8fe
KihaJeong1994 <jkha7371@gmail.com>
2022-11-24 05:42:46 +0900

    두개의 query를 이용해 querydsl을 통한 조회

--- src/main/java/com/example/jpa/repository/TeamDslRepositoryImpl.java
+++ src/main/java/com/example/jpa/repository/TeamDslRepositoryImpl.java
@@ -13,6 +13,7 @@ import static com.example.jpa.entity.QAcaCur.acaCur;
 import static com.example.jpa.entity.QClassGrp.classGrp;
 import static com.example.jpa.entity.QClassOne.classOne;
 import com.example.jpa.entity.Team;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 
@@ -29,20 +30,39 @@ public class TeamDslRepositoryImpl extends QuerydslRepositorySupport implements 
 
     @Override
     public List<Team> findAll(Pageable pageable){
+        List<Long> idList = jpaQueryFactory
+        .select(team.id)
+        .from(team)
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
         return jpaQueryFactory
                 .selectFrom(team)
                 .leftJoin(team.persons,person)
-                // .fetchJoin() //fetch join시 pagination 메모리에서 작동
+                .fetchJoin() //fetch join시 pagination 메모리에서 작동(기존 fetchjoin에서 pagination 작동 안하는 원리와 동일, 그러나 fetch join 사용 안하면 결과값이 Team의 column만 한정해서 나옴)
                 .leftJoin(person.colCur,colCur)
+                .fetchJoin() 
                 .leftJoin(colCur.classGrp,classGrp)
+                .fetchJoin() 
                 .leftJoin(classGrp.classOnes,classOne)
+                .fetchJoin() 
                 .leftJoin(colCur.classOnes,classOne)
+                .fetchJoin() 
                 .leftJoin(person.acaCur,acaCur)
+                .fetchJoin() 
                 .leftJoin(acaCur.classGrp,classGrp)
+                .fetchJoin() 
                 .leftJoin(classGrp.classOnes,classOne)
+                .fetchJoin() 
                 .leftJoin(acaCur.classOnes,classOne)
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
+                .fetchJoin() 
+                .distinct()
+                // .where(team.id.in(JPAExpressions.select(team.id)
+                //                         .from(team)
+                //                         .offset(pageable.getOffset())// sub query에서 작동안함
+                //                         .limit(pageable.getPageSize()))) // sub query에서 작동안함
+                .where(team.id.in(idList))
                 .fetch();
-    };
+    }
 }
